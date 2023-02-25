8526af25be1881495f4541956e5158bd717d13bf
yagnesh2004 <93670699+yagnesh2004@users.noreply.github.com>
2023-02-25 21:22:23 +0530

    Lot of changes has to be done in all files and displayed the images sucessfullt

--- game.js
+++ game.js
@@ -4,14 +4,21 @@ const resultDisplay = document.getElementById("result")
 let userChoice
 let computerChoice
 let result
+let userImageCode
+let code1=0
+let resulCode1,resultCode2
+
+let code2=1
 
 const possibleChoices = document.querySelectorAll('button')
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice= e.target.id
     userChoiceDisplay.innerHTML =  userChoice
+    userImageChange(userChoice)
     generatecomputerChoice()
     generateResult()
+    changeImage()
 
 }) )
 
@@ -21,19 +28,58 @@ function generatecomputerChoice(){
 
     if(randomNumber ===1){
         computerChoice ='rock'
+        randomImage(randomNumber)
+    
+
 
     }
     if(randomNumber ===2){
         computerChoice='paper'
+        randomImage(randomNumber)
 
+     
     }
     if(randomNumber ===3){
         computerChoice='scissor'
+        randomImage(randomNumber)
+
+    
         
     }
     computerChoiceDisplay.innerHTML = computerChoice
 
 }
+function userImageChange(y){
+    if(y ==='rock'){
+        userImageCode=1
+        userImageDisplay(userImageCode)
+        
+    
+
+
+    }
+    if(y ==='paper'){
+        userImageCode=2
+        userImageDisplay(userImageCode)
+
+
+        
+        
+
+     
+    }
+    if(y==='scissor'){
+        userImageCode=3
+        userImageDisplay(userImageCode)
+
+
+
+    
+        
+    }
+
+}     
+
 
 function generateResult()
 {
@@ -47,11 +93,11 @@ function generateResult()
     }
     if (computerChoice=== 'rock' && userChoice ==='scissor')
     {
-        result = 'you lost'
-    }
+        result = 'You lost'
+    }    
     if (computerChoice=== 'paper' && userChoice ==='rock')
     {
-        result = 'you win'
+        result = 'You win'
     }
     if (computerChoice=== 'paper' && userChoice ==='scissor')
     {
@@ -59,13 +105,41 @@ function generateResult()
     }
     if (computerChoice=== 'scissor' && userChoice ==='rock')
     {
-        result = 'you win'
+        result = 'You win'
     }
     if (computerChoice=== 'scissor' && userChoice ==='paper')
     {
-        result = 'you lost'
+        result = 'You lost'
     }
     resultDisplay.innerHTML = result
+}
+
+function randomImage(a){
+    var imageSource = "images/"+"img" +a + ".png";
+        var imageOriginal = document.querySelectorAll('img')[0];
+
+    imageOriginal.setAttribute('src',imageSource)
+
+
+
+
+
+}
+function userImageDisplay(z){
+       var userimageSource = "images/"+"img" +z+ ".png";
+       
+        var userimageOriginal = document.querySelectorAll('img')[1];
+
+    
+
+    
+
+      userimageOriginal.setAttribute('src',userimageSource)
+
+
+
+
+
 }
 
 
